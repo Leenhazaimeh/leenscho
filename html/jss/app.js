564e67e7b50489d63ef792dabd5ee0c8fa32732d
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-04-04 20:22:23 +0300

    added html and css and jss files

---
+++ html/jss/app.js
@@ -0,0 +1,64 @@
+alert ("thank you for choose our school" )
+alert ("let our web sit help you , answer the following qusitons pleas...")
+let num =""
+let studentnum  = prompt ("how many child you want to sign")
+function school1(studentnum){
+    {
+    if(studentnum === '1'){ document.write ("you can't have the brother disacount")}
+        else if(studentnum >='2')
+        {
+           document.write ("WLC you will have the brother disacount")
+        } 
+         else { 
+            document.write("please call us for best help")
+        }
+        document.write (num)
+   
+         
+       } console.log (typeof studentnum);
+
+    } 
+    school1(studentnum)
+
+
+
+let studentclass = prompt ("how old are they ? or in which class?")
+function school2(studentclass){
+    
+    document.write("your child age /class is:", "studentclass")}
+console.log(typeof studentclass);
+school2
+let studentgender =prompt ("girls OR boys ?")
+function shcool3(studentgender){
+    document.write(studentgender)
+}console.log(typeof studentgender)
+shcool3
+let level =''
+let studentlevel = prompt ("tell me the last degree for them ? (50 -90) ")
+
+function school(studentlevel) {
+    
+   
+ {
+ if(studentlevel === 50){ document.write ("we are sorry we can't accept this level he/her need another kind of school")}
+     else if(studentlevel <=70)
+     {
+        document.write ("WLC we have special classs to this level of student")
+     } else if ( studentlevel >= 80){
+         document.write("GREAT your son/ dughter very smart our school the best place for him/her")
+     } else { 
+         document.write("please call us for best help")
+     }
+     document.write (level)
+
+      
+    }
+    console.log (typeof studentlevel);
+
+} 
+school(studentlevel)
+
+ 
+alert ("the number of your childs is: "+ studentnum + "the old /class for them is: "+ studentclass +"the gender is:"+ studentgender+"the last degree they had " +studentlevel );
+
+alert ("thank you , call us at 07********")
